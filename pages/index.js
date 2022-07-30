6781811f1f9b80187330ff71e27676a834f26a28
serii <seriiburduja@gmail.com>
2022-07-30 11:23:42 +0300

    feat: category count

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,38 @@
-import AdminCard from '../components/admin/ui/AdminCard';
-import AdminLayout from '../layouts/AdminLayout';
-import styles from '../styles/Home.module.css';
+import { useEffect, useState } from "react";
+import AdminCard from "../components/admin/ui/AdminCard";
+import AdminLayout from "../layouts/AdminLayout";
+import axios from "axios";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
-	return (
-		<AdminLayout>
-			<div className={styles.container}>
-				<div className='admin-layout__wrap'>
-					<div className='admin-layout__item'>
-						<AdminCard title='Categories' number={3} bg="green" link="/category"  />
-					</div>
-				</div>
-			</div>
-		</AdminLayout>
-	);
+  const [categoryCount, setCategoryCount] = useState(0);
+  const getCategory = () => {
+    axios
+      .get("/category_count")
+      .then((res) => {
+        setCategoryCount(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err.response, "err.response");
+      });
+  };
+  useEffect(() => {
+    getCategory();
+  }, [categoryCount]);
+  return (
+    <AdminLayout>
+      <div className={styles.container}>
+        <div className="admin-layout__wrap">
+          <div className="admin-layout__item">
+            <AdminCard
+              title="Categories"
+              number={categoryCount}
+              bg="green"
+              link="/category"
+            />
+          </div>
+        </div>
+      </div>
+    </AdminLayout>
+  );
 }
